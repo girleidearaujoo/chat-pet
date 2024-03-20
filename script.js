42c8e5f4f29067e0659d08dabe7d43fd61a65a30
Girleide de Araujo <115583142+girleidearaujoo@users.noreply.github.com>
2024-03-20 09:06:10 -0300

    ...

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form')
+const form_emp = document.getElementById('form-emp')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    let input = document.getElementById('inputState')
+    let inputTurn = document.getElementById('inputStateTurn')
+    let inputDate = document.getElementById('inputDate')
+    let div =  document.getElementById('div')
+    desenharCards(div, verificarEmpresas( input.value, inputDate.value));
+})
